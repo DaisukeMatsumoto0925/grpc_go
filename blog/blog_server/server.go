a6f15ce6cb2577d6aa47b2ee16bafa347d763636
DaisukeMatsumoto0925 <daisuke.matsumoto0925@gmail.com>
2022-03-26 09:00:11 +0900

    set up

---
+++ blog/blog_server/server.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+
+	"github.com/DaisukeMatsumoto0925/blog/blogpb"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"google.golang.org/grpc"
+)
+
+var collection *mongo.Collection
+
+type server struct {
+	blogpb.UnimplementedBlogServiceServer
+}
+
+type blogItem struct {
+	ID       primitive.ObjectID `bson:"_id,omitempty"`
+	AuthorID string             `bson:"author_id"`
+	Content  string             `bson:"content"`
+	Title    string             `bson:"title"`
+}
+
+func main() {
+	log.SetFlags(log.Lshortfile | log.Lshortfile)
+
+	// connect to MongoDB
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://root:password123@localhost:27017"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = client.Connect(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("connected DB")
+
+	collection = client.Database("mydb").Collection("blogs")
+
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	blogpb.RegisterBlogServiceServer(s, &server{})
+
+	go func() {
+		fmt.Println("Starting Server...")
+		if err := s.Serve(lis); err != nil {
+			log.Fatalf("failed to serve: %v", err)
+		}
+	}()
+
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt)
+
+	<-ch
+	fmt.Println("Stopping the server")
+	s.Stop()
+	fmt.Println("Closing the listener")
+	lis.Close()
+	fmt.Println("Closing mongoDB")
+	client.Disconnect(context.TODO())
+	fmt.Println("End of Program")
+
+}
