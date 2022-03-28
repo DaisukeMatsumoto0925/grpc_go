3b50d22ab9498342111603f68f406ea6e8a0f006
DaisukeMatsumoto0925 <daisuke.matsumoto0925@gmail.com>
2022-03-28 11:28:32 +0900

    updated

--- blog/blog_client/client.go
+++ blog/blog_client/client.go
@@ -1,1 +1,38 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/DaisukeMatsumoto0925/blog/blogpb"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	fmt.Println("Blog Client")
+
+	opts := grpc.WithInsecure()
+
+	cc, err := grpc.Dial("localhost:50051", opts)
+	if err != nil {
+		log.Fatalf("could not connect: %v", err)
+	}
+	defer cc.Close()
+
+	c := blogpb.NewBlogServiceClient(cc)
+
+	fmt.Println("Creating Blog")
+	blog := &blogpb.Blog{
+		AuthorId: "Daisuke",
+		Title:    "My First Blog",
+		Content:  "Content of the first blog",
+	}
+
+	res, err := c.CreateBlog(context.Background(), &blogpb.CreateBlogRequest{Blog: blog})
+	if err != nil {
+		log.Fatalf("Unexpected error: %v", err)
+	}
+	fmt.Printf("Blog has created: %v", res)
+
+}
--- blog/go.mod
+++ blog/go.mod
@@ -3,6 +3,7 @@ module github.com/DaisukeMatsumoto0925/blog
 go 1.17
 
 require (
+	go.mongodb.org/mongo-driver v1.8.4
 	google.golang.org/grpc v1.45.0
 	google.golang.org/protobuf v1.28.0
 )
@@ -17,7 +18,6 @@ require (
 	github.com/xdg-go/scram v1.0.2 // indirect
 	github.com/xdg-go/stringprep v1.0.2 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
-	go.mongodb.org/mongo-driver v1.8.4 // indirect
 	golang.org/x/crypto v0.0.0-20201216223049-8b5274cf687f // indirect
 	golang.org/x/net v0.0.0-20200822124328-c89045814202 // indirect
 	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
